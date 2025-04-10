943cda7131d6e456ed1da98b6278c3855302b4ba
RanzD <103300191+RanzD@users.noreply.github.com>
2025-04-10 22:47:19 +0700

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'April 14, 2025',
+  birthdate: 'April 17, 2025',
   name: 'Chelsea'
 };
 
